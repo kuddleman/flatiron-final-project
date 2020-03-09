aa8ccc871c202b194f1c6d8b2c011492f95a6e2a
Kuddleman <edl1155@gmail.com>
2020-03-09 15:56:07 -0700

    add validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,52 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    
   end
 
   def new
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
+  def edit
+ 
+  end
+
+  def update
+    
+    @product.update(product_params)
+    redirect_to root_path
+  end
+
+  def destroy
+   
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
- 
+  def find_product
+    begin
+    @product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :image_url)
+  end
     
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :name, :description, presence: true
+  validates :price, numericality: { greater_than: 0.0}
 end
