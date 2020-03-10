52cbd15206a397fba3e4ce6de537d7884cc9b97c
Kuddleman <edl1155@gmail.com>
2020-03-10 10:55:35 -0700

    add regex code to validate formating of email in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, 
+    presence: true, 
+    uniqueness: { case_sensitive: false },
+    format: { 
+      with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i 
+    }
   has_secure_password
 end
