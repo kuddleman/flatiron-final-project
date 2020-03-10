a95767fa8b1bafe9bd5b9a51b7ccd68923e11239
Kuddleman <edl1155@gmail.com>
2020-03-10 15:35:49 -0700

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Signed up successfully"
+      redirect_to root_path
+    else  
+      flash.now[:alert] = "Sign up failed"
+      render :new
+    end
   end
-end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+  end
+
+  
+
