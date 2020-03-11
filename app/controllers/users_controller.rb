c7bc50a1db4505656e78e7b03f05c342c1404fdb
Kuddleman <edl1155@gmail.com>
2020-03-11 16:44:30 -0700

    prevent signup if signed in in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if current_user
+      flash[:notice] = "You have already signed up"
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
