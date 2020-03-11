fcc29c71cc6a104d0c43872d60cb01a4ca252065
Kuddleman <edl1155@gmail.com>
2020-03-11 10:57:31 -0700

    add functionality in create action in sessions_controller to authenticate sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # 1. find user by email address and downcase it
+    user = User.find_by( email: params[:email].downcase )
+
+    # 2. Check if the user can be authenticated using the password provided
+    if user && user.authenticate(params[:password])
+      cookies.signed[:user_id] = user.user_id
+      flash[:notice] = "Sign in successful!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Invalid email/password combination"
+      render :new
+    end  
+    # 3. If a user is found and authenticated, then set signed cookie
+
+    # 4. Otherwise, set alert message and render new form
   end
 
   def destroy
