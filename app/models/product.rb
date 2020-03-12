797498bdb8930a27f99ae1854a3b047e45a64314
Kuddleman <edl1155@gmail.com>
2020-03-11 19:15:02 -0700

    add user id and quantity available to product model by creating migration

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
   validates :name, :description, presence: true
-  validates :price, numericality: { greater_than: 0.0}
+  validates :price, numericality: { 
+    greater_than: 0.0
+  }
+  validates :quantity, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+
+
+  belongs_to :user
 end
