c7d837d9f7ba7c9947f742ba5ce576bd973e0ba5
Kuddleman <edl1155@gmail.com>
2020-03-11 19:49:41 -0700

    whitelist the quantity field in Product..add quantity to params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -56,7 +56,10 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :description, :image_url)
+    params.require(:product).permit(
+      :name, :price, :description, 
+      :image_url, :quantity
+      )
   end
     
 end
